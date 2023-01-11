6781f122b0418261347155bc7fb8b0eadbf2e4ba
Hülya Büyükarslan <monaroza825@gmail.com>
2023-01-11 23:36:13 +0300

    11.01.2023

---
+++ src/test/java/karisik_practice/Ortaya_Karisik.java
@@ -0,0 +1,14 @@
+package karisik_practice;
+
+import junit_my_practice.TestBase;
+
+public class Ortaya_Karisik extends TestBase {
+    // https://amazon.com adresine gidiniz
+    // sag ust bolumde bulunan dil secenek menusunun acilmasi icin mause'u bu menunun ustune getirelim
+    // Change country/region butonuna basiniz
+    // United States dropdown'undan 'Turkey (Türkiye)' seciniz
+    // Go to website butonuna tiklayiniz
+    // acilan yeni sayfadanin Title'inin Elektronik icerdigini test ediniz
+
+
+}
